ed569c378a04753cfc641f37411b8cdf14429025
Nightey404 <trolshakyal@gmail.com>
2026-08-03 15:08:59 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Register from "./components/register";
+
+const App = () => {
+  return (
+    <div>
+      <Register message="Welcome to the Register page!" />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const register = ({ message }) => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default register;
